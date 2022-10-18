8266a089feb86a3beb0dcd8f8801edd1a4f370d7
Frank Calise <fcalise@gmail.com>
2022-10-17 23:44:13 -0400

    config plugin info plist

--- withReactNativeShareMenu.ts
+++ withReactNativeShareMenu.ts
@@ -1,11 +1,23 @@
 import type { ConfigPlugin } from "@expo/config-plugins";
+import plist from "@expo/plist";
 import {
   withAndroidManifest,
   withEntitlementsPlist,
+  InfoPlist,
+  withDangerousMod,
 } from "@expo/config-plugins";
+import * as fs from "fs";
+import * as path from "path";
 
-const withReactNativeShareMenu: ConfigPlugin = (expoConfig) => {
-  return withShareMenuEntitlements(expoConfig);
+export function getProjectStickersName(name: string) {
+  return `${name}ShareMenu`;
+}
+
+const withReactNativeShareMenu: ConfigPlugin = (config) => {
+  config = withShareMenuEntitlements(config);
+  config = withShareMenuInfoPlist(config);
+
+  return config;
 };
 // withAndroidManifest(expoConfig, async (modConfig) => {
 //   let androidManifest = modConfig.modResults.manifest;
@@ -22,6 +34,42 @@ const withShareMenuEntitlements: ConfigPlugin = (config) => {
     ];
     return config;
   });
+};
+
+const withShareMenuInfoPlist: ConfigPlugin = (config) => {
+  return withDangerousMod(config, [
+    "ios",
+    async (config) => {
+      const shareMenuExtName = getProjectStickersName(
+        config.modRequest.projectName!
+      );
+      const shareMenuRootPath = path.join(
+        config.modRequest.platformProjectRoot,
+        shareMenuExtName
+      );
+      const filePath = path.join(shareMenuRootPath, "Info.plist");
+
+      const shareMenu: InfoPlist = {
+        HostAppBundleIdentifier: "$(PRODUCT_BUNDLE_IDENTIFIER)",
+        HostAppURLScheme: "$(PRODUCT_BUNDLE_IDENTIFIER)://",
+        NSExtension: {
+          NSExtensionAttributes: [
+            {
+              NSExtensionActivationRule: [
+                { NSExtensionActivationSupportsText: true },
+                { NSExtensionActivationSupportsWebURLWithMaxCount: 1 },
+              ],
+            },
+          ],
+        },
+      };
+
+      await fs.promises.mkdir(path.dirname(filePath), { recursive: true });
+      await fs.promises.writeFile(filePath, plist.build(shareMenu));
+
+      return config;
+    },
+  ]);
 };
 
 export default withReactNativeShareMenu;
